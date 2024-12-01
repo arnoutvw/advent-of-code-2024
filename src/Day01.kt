936c7bba696cc9be764a38f2a79a367e2dd8acbe
Arnout van Westen <arnout.van.westen@conspect.nl>
2024-12-01 10:37:13 +0100

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,45 @@
 fun main() {
+
+    fun createLeftRightLists(input: List<String>): Pair<ArrayList<Int>, ArrayList<Int>> {
+        val left = ArrayList<Int>()
+        val right = ArrayList<Int>()
+        input.forEach {
+            val split = it.split("\\s+".toRegex())
+            left.add(split[0].toInt())
+            right.add(split[1].toInt())
+        }
+        left.sort()
+        right.sort()
+        return Pair(left, right)
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        var totalDistance = 0;
+
+        val (left, right) = createLeftRightLists(input)
+
+        left.forEachIndexed { index, value ->
+            totalDistance += Math.abs(value - right[index])
+        }
+
+        return totalDistance
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
-    }
+        var totalSim = 0
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+        val (left, right) = createLeftRightLists(input)
+
+        left.forEach { value ->
+            totalSim += value * right.count{ it == value }
+        }
+
+        return totalSim
+    }
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 11)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
